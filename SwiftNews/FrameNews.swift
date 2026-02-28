d20b924e5caba9a373d26e7544af1c2c12b8b7c7
Vladyslove03 <nadaxovskiy@gmail.com>
2026-02-28 14:38:46 +0200

    Practiced frame alignments, fixed sizes, and infinity constraints

---
+++ SwiftNews/FrameNews.swift
@@ -0,0 +1,31 @@
+//
+//  FrameNews.swift
+//  SwiftNews
+//
+//  Created by Влад Надаховский on 28.02.2026.
+//
+
+import SwiftUI
+
+struct FrameNews: View {
+    var body: some View {
+        Text("Hello, World!")
+            .background(Color.red)
+            .frame(height: 100, alignment: .top)
+            .background(Color.orange)
+            .frame(width: 150)
+            .background(Color.purple)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color.pink)
+            .frame(height: 400)
+            .background(Color.green)
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(Color.yellow)
+    }
+}
+
+struct FrameNews_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameNews()
+    }
+}
