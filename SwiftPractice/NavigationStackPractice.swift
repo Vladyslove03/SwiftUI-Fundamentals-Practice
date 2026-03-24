18cd610af8fb2ab4935eb652033df75bbcbfa342
Vladyslove03 <nadaxovskiy@gmail.com>
2026-03-24 20:02:49 +0200

    feat: implement @FocusState with enum for advanced form navigation

---
+++ SwiftPractice/NavigationStackPractice.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationStackPractice.swift
+//  SwiftPractice
+//
+//  Created by User on 01.04.2026.
+//
+
+import SwiftUI
+
+struct NavigationStackPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStackPractice()
+}
