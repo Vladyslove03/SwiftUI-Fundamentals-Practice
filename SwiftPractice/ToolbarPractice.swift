4e13d8f478d373660de1615bc44ffd8b2386f745
Vladyslove03 <nadaxovskiy@gmail.com>
2026-04-01 13:42:04 +0300

    fix: restore project files and update project settings

--- SwiftPractice/ToolbarPractice.swift
+++ SwiftPractice/ToolbarPractice.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct ToolbarPractice: View {
+    
+    @State private  var text: String = ""
+    
+    @State private var paths: [String] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(path: $paths) {
+            ZStack {
+                Color.white.ignoresSafeArea()
+                
+                ScrollView {
+                    TextField("Placeholder", text: $text)
+                    
+                    ForEach(0..<50) { _ in
+                        Rectangle()
+                            .frame(width: 200, height: 200)
+                    }
+                }
+            }
+            .navigationTitle("Toolbar")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image(systemName: "heart.fill")
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image(systemName: "house.fill")
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarTitleMenu {
+                Button("Screen 1") {
+                    paths.append("Screen 1")
+                }
+                Button("Screen 1") {
+                    paths.append("Screen 2")
+                }
+            }
+            .navigationDestination(for: String.self) { value in
+                Text("New screen: \(value)")
+            }
+        }
     }
 }
-
 #Preview {
     ToolbarPractice()
 }
