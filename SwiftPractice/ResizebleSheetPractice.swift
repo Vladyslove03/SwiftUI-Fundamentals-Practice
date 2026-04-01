5412fbc0888a4c34d1f1ac53444b5d210ed1f463
Vladyslove03 <nadaxovskiy@gmail.com>
2026-04-01 14:11:03 +0300

    feat: add resizable sheet practice with presentation detents

---
+++ SwiftPractice/ResizebleSheetPractice.swift
@@ -0,0 +1,18 @@
+//
+//  ResizebleSheetPractice.swift
+//  SwiftPractice
+//
+//  Created by User on 01.04.2026.
+//
+
+import SwiftUI
+
+struct ResizebleSheetPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResizebleSheetPractice()
+}
