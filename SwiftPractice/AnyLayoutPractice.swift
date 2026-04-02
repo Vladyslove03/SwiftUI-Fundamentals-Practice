918316b50e175172dda49be4a3c9a04972faad0e
Vladyslove03 <nadaxovskiy@gmail.com>
2026-04-02 17:45:41 +0300

    Practice AnyLayout with size class based view arrangement

---
+++ SwiftPractice/AnyLayoutPractice.swift
@@ -0,0 +1,18 @@
+//
+//  AnyLayoutPractice.swift
+//  SwiftPractice
+//
+//  Created by User on 02.04.2026.
+//
+
+import SwiftUI
+
+struct AnyLayoutPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnyLayoutPractice()
+}
