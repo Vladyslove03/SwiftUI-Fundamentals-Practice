a7f42ae0473c44c18a5f61f413ef3a47c09378a3
Vladyslove03 <nadaxovskiy@gmail.com>
2026-04-02 12:12:28 +0300

    feat: implement native popover with adaptive presentation

---
+++ SwiftPractice/NativePopoverPractice.swift
@@ -0,0 +1,18 @@
+//
+//  NativePopoverPractice.swift
+//  SwiftPractice
+//
+//  Created by User on 02.04.2026.
+//
+
+import SwiftUI
+
+struct NativePopoverPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NativePopoverPractice()
+}
