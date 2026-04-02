9fe6b5d56756b795ec51dafc2a75c648b4dcf69b
Vladyslove03 <nadaxovskiy@gmail.com>
2026-04-02 18:25:59 +0300

    feat: add Grid practice with custom cell spanning and alignment

---
+++ SwiftPractice/GridViewPractice.swift
@@ -0,0 +1,18 @@
+//
+//  GridViewPractice.swift
+//  SwiftPractice
+//
+//  Created by User on 02.04.2026.
+//
+
+import SwiftUI
+
+struct GridViewPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridViewPractice()
+}
