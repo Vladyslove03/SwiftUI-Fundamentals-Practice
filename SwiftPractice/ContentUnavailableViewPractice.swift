df3096800c1a1700b4d193da7174581c6c0e59ed
Vladyslove03 <nadaxovskiy@gmail.com>
2026-04-03 10:03:42 +0300

    Add ContentUnavailableView

---
+++ SwiftPractice/ContentUnavailableViewPractice.swift
@@ -0,0 +1,18 @@
+//
+//  ContentUnavailableViewPractice.swift
+//  SwiftPractice
+//
+//  Created by User on 03.04.2026.
+//
+
+import SwiftUI
+
+struct ContentUnavailableViewPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentUnavailableViewPractice()
+}
